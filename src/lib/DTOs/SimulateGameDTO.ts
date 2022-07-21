75388db34e1b3c60dca5134d54046c09308e4bf5
Ewanderson <inovaprog@gmail.com>
2022-07-20 23:24:06 -0300

    feat[DTO]: create DTO

--- src/lib/DTOs/SimulateGameDTO.ts
+++ src/lib/DTOs/SimulateGameDTO.ts
@@ -1,10 +1,39 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Behaviors } from '../entities/player';
 
 export class SimulateGameDto {
   @IsOptional()
   @IsString({ each: true })
   @IsNotEmpty()
-  behaviors: string;
+  behaviors?: Behaviors[];
 
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  max_round?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  max_value?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  min_value?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  number_of_properties?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  start_balance?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsNotEmpty()
+  rent_percent?: number;
 }
